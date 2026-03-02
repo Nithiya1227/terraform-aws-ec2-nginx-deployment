7d91cef03e6372722fcd42c5ad22a1c52b0f7b57
Nithiya Bharathi <nithyabharathi77@gmail.com>
2026-03-02 17:49:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS Region"
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
